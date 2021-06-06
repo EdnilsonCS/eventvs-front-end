d118dcbb7dc05cb33f9260ea1b20e7783f5bc23a
ednilsoncs <ednilsonsantos43@gmail.com>
2021-06-06 20:31:03 -0300

    chore:  adiciona validação

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,18 +1,46 @@
 import React from 'react';
+import { Controller, FieldErrors } from 'react-hook-form';
+import { TextInputProps } from 'react-native';
 import { BaseInput, Container, Error } from './styles';
 
-export default function Input({ errorText, ...props }: any): JSX.Element {
-  return (
-    <Container>
-      <BaseInput
-        mode="outlined"
-        returnKeyType="next"
-        underlineColor="transparent"
-        selectionColor="#AAA"
-        left={<BaseInput.Icon {...props} />}
-        {...props}
-      />
-      {errorText ? <Error>{errorText}</Error> : null}
-    </Container>
-  );
+interface InputProps extends TextInputProps {
+  name: string;
+  icon: string;
+  control: any;
+  errors: FieldErrors;
 }
+
+const Input: React.FC<InputProps> = ({
+  errors,
+  name,
+  control,
+  icon,
+  ...props
+}) => {
+  return (
+    <Controller
+      control={control}
+      render={({ field: { onChange, onBlur, value } }) => (
+        <Container>
+          <BaseInput
+            onChange={onChange}
+            onBlur={onBlur}
+            mode="outlined"
+            returnKeyType="next"
+            underlineColor="transparent"
+            selectionColor="#AAA"
+            value={value}
+            left={<BaseInput.Icon name={icon} />}
+            {...props}
+          />
+          {errors[name] ? <Error>{errors[name].message}</Error> : null}
+        </Container>
+      )}
+      name={name}
+      rules={{ required: true }}
+      defaultValue=""
+    />
+  );
+};
+
+export default Input;
--- src/screens/onboarding/Signin/index.tsx
+++ src/screens/onboarding/Signin/index.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import MaterialCommunityiIcons from 'react-native-vector-icons/MaterialCommunityIcons';
-
+import { useForm } from 'react-hook-form';
+import * as Yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
 import Input from '@components/Input';
 import { SignInScreenProps } from '@routes/public.routes';
+import { useAuth } from '@hooks/auth';
+import { PublicRoutesConstants } from '@routes/constants.routes';
 import {
   ButtonContainer,
   LoginButton,
@@ -13,31 +17,57 @@ import {
 } from './styles';
 
 export default function Signin({ navigation }: SignInScreenProps): JSX.Element {
+  const { signIn } = useAuth();
+  const schema = Yup.object().shape({
+    email: Yup.string().email().required('E-mail obrigatório'),
+    password: Yup.string().required('Senha obrigatória'),
+  });
+
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+    mode: 'onBlur',
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+  });
+  const handleLogin = async (data: any): Promise<void> => {
+    await signIn(data);
+  };
   return (
     <Container>
       <Header>EventVS</Header>
       <Input
+        errors={errors}
+        control={control}
+        name="email"
         label="Email"
         autoCapitalize="none"
-        name="account"
+        icon="account"
         color="#6d43a1"
       />
       <Input
+        errors={errors}
+        control={control}
+        name="password"
         label="Senha"
         autoCapitalize="none"
         secureTextEntry
-        name="lock"
+        icon="lock"
         color="#6d43a1"
       />
       <ButtonContainer>
-        <LoginButton
-          mode="contained"
-          onPress={() => navigation.push('Navigation')}
-        >
+        <LoginButton mode="contained" onPress={handleSubmit(handleLogin)}>
           Login
         </LoginButton>
       </ButtonContainer>
-      <CreateAccountContainer onPress={() => navigation.push('CreateAccount')}>
+      <CreateAccountContainer
+        onPress={() => navigation.push(PublicRoutesConstants.CreateAccount)}
+      >
         <CreateAccountText>Criar nova conta</CreateAccountText>
         <MaterialCommunityiIcons name="arrow-right" color="#000" size={20} />
       </CreateAccountContainer>
