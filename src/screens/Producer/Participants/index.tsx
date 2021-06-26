5470edacca0930c81f199e944a2b009686f2a6bd
ednilsoncs <ednilsonsantos43@gmail.com>
2021-06-25 23:25:49 -0300

    feat: inciado tela de participante

--- src/screens/Producer/Participants/index.tsx
+++ src/screens/Producer/Participants/index.tsx
@@ -1,9 +1,67 @@
-import React from 'react';
+import { useRoute } from '@react-navigation/native';
+import { Text } from 'react-native-paper';
+import EventService, { IEvent } from '@services/EventService';
+import React, { useMemo, useState, useEffect } from 'react';
+import { View } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import dayjs from '@helpers/datas';
+import {
+  Bold,
+  Button,
+  Container,
+  Card,
+  Header,
+  Title,
+  Wrapper,
+  ButtonContainer,
+  NameParticipante,
+} from './styles';
 
-import { Container } from './styles';
+interface RouteParams {
+  id: number;
+}
+const Published: React.FC = () => {
+  const [dados, setDados] = useState<IEvent | null>(null);
+  const [participantes, setParticipantes] = useState([]);
+  const { params } = useRoute();
+  const routeParams = params as RouteParams;
+  const navigation = useNavigation();
+  useEffect(() => {
+    const getEventDetail = async (): Promise<void> => {
+      const data = await EventService.getEventDetail(routeParams.id);
+      setDados(data);
+    };
 
-const Participants: React.FC = () => {
-  return <Container />;
+    getEventDetail();
+  }, [routeParams.id]);
+
+  return (
+    <Container>
+      <Header>Detalhes</Header>
+      <Card>
+        <View>
+          <Wrapper>
+            <Title>
+              <Bold>{dados?.nome}</Bold>
+            </Title>
+          </Wrapper>
+        </View>
+        <View>
+          {participantes.map(item => (
+            <NameParticipante>{item.name}</NameParticipante>
+          ))}
+        </View>
+      </Card>
+      <ButtonContainer>
+        <Button
+          style={{ backgroundColor: '#DE0b20', marginBottom: 14 }}
+          onPress={() => navigation.goBack()}
+        >
+          Voltar
+        </Button>
+      </ButtonContainer>
+    </Container>
+  );
 };
 
-export default Participants;
+export default Published;
--- src/screens/Producer/Participants/styles.ts
+++ src/screens/Producer/Participants/styles.ts
@@ -1,3 +1,47 @@
 import styled from 'styled-components/native';
+import { Button as PaperButton } from 'react-native-paper';
 
-export const Container = styled.View``;
+export const Container = styled.View`
+  padding: 0 16px;
+`;
+export const Card = styled.View`
+  border-width: 1px;
+  border-color: #6d43a1;
+  border-radius: 10px;
+  height: 300px;
+  justify-content: space-between;
+  margin-bottom: 32px;
+  padding: 0px 18px;
+`;
+
+export const Wrapper = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  margin: 8px 0;
+`;
+
+export const Title = styled.Text`
+  color: #6d43a1;
+  font-size: 20px;
+`;
+
+export const Bold = styled.Text`
+  font-weight: bold;
+`;
+
+export const Header = styled.Text`
+  align-self: center;
+  font-size: 42px;
+  font-weight: bold;
+  font-family: 'Lato';
+  margin: 20px 0 20px;
+`;
+export const ButtonContainer = styled.View`
+  margin: 20px 0px;
+`;
+
+export const Button = styled(PaperButton).attrs(() => ({
+  mode: 'contained',
+}))``;
+
+export const NameParticipante = styled.Text``;
