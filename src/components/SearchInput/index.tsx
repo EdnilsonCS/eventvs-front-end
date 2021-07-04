20e62f06a04233b5291f495051e12b0f8ecdf82e
ednilsoncs <ednilsonsantos43@gmail.com>
2021-07-04 16:30:05 -0300

    feat: adicionado action para alteração do input de pesquisa

--- src/components/SearchInput/index.tsx
+++ src/components/SearchInput/index.tsx
@@ -2,10 +2,14 @@ import React from 'react';
 
 import { Container, Input } from './styles';
 
-function SearchInput({ ...props }: any): JSX.Element {
+interface ISearchInput {
+  onChangeText(e: string): void;
+}
+
+function SearchInput({ onChangeText, ...props }: ISearchInput): JSX.Element {
   return (
     <Container>
-      <Input {...props} />
+      <Input {...props} onChangeText={onChangeText} />
     </Container>
   );
 }
